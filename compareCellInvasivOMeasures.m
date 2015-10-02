4b244e328c55ac26f4c65d330515b9886c3d3e39
Marilisa Cortesi <marilisa.cortesi2@unibo.it>
2015-10-02 15:43:52 +0200

    Update compareCellInvasivOMeasures.m

--- compareCellInvasivOMeasures.m
+++ compareCellInvasivOMeasures.m
@@ -1,3 +1,20 @@
+%     compare-Cell Invasiv-o-Measures: fuction that analyzes the outputs of Cell-Invasiv-O-Meter.
+%     Copyright (C) 2015  Marilisa Cortesi
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 2 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 function [areaDecrease,invasivity]=compareCellInvasivOMeasures(folderMat,control, conditions,timepoints)
 currentFolder=pwd;
 cd(folderMat)
